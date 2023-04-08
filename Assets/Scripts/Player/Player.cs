5975d4a4625ac8e1900df994fe61a6f4f7c21618
Heruwim <heruwim25@gmail.com>
2023-04-08 12:38:30 +0300

    Add script Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] private int _health;
+
+    public void ApplyDamage(int damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+
+    }
 }
