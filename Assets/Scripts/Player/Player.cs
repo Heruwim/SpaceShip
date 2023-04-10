81a2e7fc6d485891a3f809588a3fa77c7147a6d1
Heruwim <heruwim25@gmail.com>
2023-04-10 10:52:13 +0300

    Implemented player control and shooting

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _health;
+    [SerializeField] private List<Weapon> _weapons;
+    [SerializeField] private Transform _shootPoint;
+
+    private Weapon _currentWeapon;
+    private int _currentHealth;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _currentWeapon = _weapons[0];
+        _currentHealth = _health;
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Shoot()
+    {
+        _currentWeapon.Shoot(_shootPoint);
+    }
 
     public void ApplyDamage(int damage)
     {
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private PlayerInput _input;
+    private Player _player;
+
+    private void Awake()
+    {
+        _input = new PlayerInput();
+
+        _player = GetComponent<Player>();
+
+        _input.Player.Shoot.performed += context => _player.Shoot();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 moveDirection = _input.Player.Move.ReadValue<Vector2>();
+
+        Move(moveDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        float scaleMoveSpeed = _speed * Time.deltaTime;
+
+        Vector3 moveDirection = new Vector3(direction.x, direction.y, 0);
+        transform.position += moveDirection * scaleMoveSpeed;
+    }   
+}
